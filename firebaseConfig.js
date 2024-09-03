19fcbcaa5b04d0989b4867d44e5a1d3e8fcefca3
reactAppAssistant <82675519+reactAppAssistant@users.noreply.github.com>
2024-09-03 18:59:53 +0530

    upload/image/video apis final

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,33 @@
-// firebaseConfig.js
+// // firebaseConfig.js
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./beni-media-storage-firebase-adminsdk-5vwio-a96567ab4f.json');
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: 'beni-media-storage.appspot.com',
+// });
+
+// const bucket = admin.storage().bucket();
+
+// module.exports = { bucket };
+
+
 const admin = require('firebase-admin');
-const serviceAccount = require('./beni-media-storage-firebase-adminsdk-5vwio-98d4a9fff0.json');
+require('dotenv').config();
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
